e3e2e391a82d95ba18d10d77ae0d9af9d947b90f
François Jacquet <francoisjacquet@users.noreply.github.com>
2016-02-29 14:42:34 -0500

    Format Index template code & fix Read more link.

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
-<?php 
+<?php
 /**
- * 
+ * Index template
+ *
+ * @package Premise WP Theme
  */
 
 get_header();
@@ -8,40 +10,40 @@ get_header();
 ?>
 
 <section id="content">
-	<?php 
+	<?php
 	if ( have_posts() ) : ?>
 	<div class="the-loop">
 		<div class="premise-row">
-			<?php 
+			<?php
 			while ( have_posts() ) : the_post(); ?>
-		 		<article <?php post_class( 'col4 premise-flip' ); ?>>
+				<article <?php post_class( 'col4 premise-flip' ); ?>>
 
-		 				<div class="the-post-content">
-				 			<a href="<?php the_permalink(); ?>" class="the-permalink premise-block">
-					 			<?php 
-					 			if( has_post_thumbnail() ) : ?>
-					 				<div class="post-thumbnail">
-					 					<?php the_post_thumbnail( 'full', array( 'class' => 'premise-responsive' ) ); ?>
-					 				</div>
-					 			<?php endif; ?>
-				 				<div class="the-post-title">
-					 				<h2><?php the_title(); ?></h2>
-					 			</div>
-				 			</a>
-				 			<span class="the-post-excerpt">
-				 				<?php the_content('Read More'); ?>
-				 			</span>
-				 		</div>
+						<div class="the-post-content">
+							<a href="<?php the_permalink(); ?>" class="the-permalink premise-block">
+								<?php
+								if ( has_post_thumbnail() ) : ?>
+									<div class="post-thumbnail">
+										<?php the_post_thumbnail( 'full', array( 'class' => 'premise-responsive' ) ); ?>
+									</div>
+								<?php endif; ?>
+								<div class="the-post-title">
+									<h2><?php the_title(); ?></h2>
+								</div>
+							</a>
+							<span class="the-post-excerpt">
+								<?php echo apply_filters('the_content_more_link', get_the_content( 'Read more' ) );  ?>
+							</span>
+						</div>
 
-		 		</article>
-			<?php 
+				</article>
+			<?php
 			endwhile; ?>
 		</div>
 	</div>
-	<?php 
+	<?php
 	else : ?>
 
-	<?php 
+	<?php
 	endif; ?>
 </section>
 
